b5bf14abd0db7e8d5911fd09d0a044331773c360
Artem Palamarchuk <artem.palamarchuk.96@gmail.com>
2017-06-05 17:49:58 +0300

    Update dependencies and make small fixes

--- src/app/services/data.service.spec.ts
+++ src/app/services/data.service.spec.ts
@@ -1,7 +1,7 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
-import { DataService } from './data.service.ts';
+import { DataService } from './data.service';
 
 describe('DataService', () => {
   beforeEach(() => {
